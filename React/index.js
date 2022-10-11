aeb85af96f1a76e07c7b97cc75e41aac99e610bc
rabbitlopez77 <rabbitlopez22@gmail.com>
2022-10-10 22:43:30 -0700

    kk

---
+++ React/index.js
@@ -0,0 +1,7 @@
+ReactDOM.render(<h1>Hello, er body</h1>, document.getElementById("root"))
+function MainContent() {
+    return(
+        <h1>I'm learning React</h1>
+    )
+}
+ReactDOM.render(<MainContent />, document.getElementById("root"))
