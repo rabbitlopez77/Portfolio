00818c0deb1519d5d11db61870e697d5d37952dd
rabbitlopez77 <rabbitlopez22@gmail.com>
2022-10-11 20:59:43 -0700

    kk

---
+++ React/Header.js
@@ -0,0 +1,16 @@
+
+
+export default function Header() {
+    return (
+        <header>
+            <nav className="nav">
+                <img src="./react-logo.png" width="40px" />
+                <ul className="list">
+                    <li>1</li>
+                    <li>2</li>
+                    <li>3</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
